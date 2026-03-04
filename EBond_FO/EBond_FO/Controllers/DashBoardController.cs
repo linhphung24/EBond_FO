e0e71fe71c6fee56fe18ab4c5d4be17f5310b152
Linh Phùng <phungduclinh.99@gmail.com>
2026-03-04 16:24:45 +0700

    add layout and dashboard

---
+++ EBond_FO/EBond_FO/Controllers/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EBond_FO.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
