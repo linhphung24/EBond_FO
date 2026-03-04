f7e854a945844b2338e13176314410c2a5302a0c
Linh Phùng <phungduclinh.99@gmail.com>
2026-03-04 16:55:02 +0700

    update

---
+++ EBond_FO/EBond_FO/Controllers/AnalyticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EBond_FO.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
