31f25d0425bf952e8bd57c1a7083f73d9ab0f20b
quocdat6204 <quocdat6204@gmail.com>
2026-03-13 16:24:48 +0700

    update Dashboard

--- EBond_FO/EBond_FO/Controllers/DashBoardController.cs
+++ EBond_FO/EBond_FO/Controllers/DashBoardController.cs
@@ -13,7 +13,7 @@ namespace EBond_FO.Controllers
             _http = factory.CreateClient("API");
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string keyword, string securityTradingStatus)
         {
             var accessToken = HttpContext.Session.GetString("AccessToken");
 
@@ -24,7 +24,38 @@ namespace EBond_FO.Controllers
             _http.DefaultRequestHeaders.Authorization =
                 new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
 
-            var res = await _http.GetAsync("/api/bond/getall");
+            // Lấy danh sách tất cả các trạng thái
+            var status = await _http.GetAsync("/api/Dashboard/mccode/security-trading-status");
+
+            var listStatus = new List<MCCode>();
+
+            if (status.IsSuccessStatusCode)
+            {
+                listStatus = await status.Content.ReadFromJsonAsync<List<MCCode>>(
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true })
+                    ?? new List<MCCode>();
+            }
+
+            // Lấy danh sách các trạng thái để tạo dropdown
+            ViewBag.AllStatus = listStatus;
+
+            // Build query string
+            var url = "/api/Dashboard/bond/getall";
+
+            var query = new List<string>();
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                query.Add($"search={keyword}");
+            }
+
+            if (!string.IsNullOrEmpty(securityTradingStatus))
+                query.Add($"securityTradingStatus={securityTradingStatus}");
+
+            if (query.Any())
+                url += "?" + string.Join("&", query);
+
+            var res = await _http.GetAsync(url);
 
             if (!res.IsSuccessStatusCode)
                 return View(Enumerable.Empty<IFG_Corporate_Bond_Info>());
@@ -32,6 +63,20 @@ namespace EBond_FO.Controllers
             var list = await res.Content.ReadFromJsonAsync<List<IFG_Corporate_Bond_Info>>(
                 new JsonSerializerOptions { PropertyNameCaseInsensitive = true })
                 ?? new List<IFG_Corporate_Bond_Info>();
+
+
+            // Map status text
+            foreach (var bond in list)
+            {
+                var statusMatch = listStatus
+                    .FirstOrDefault(x => x.ID == bond.SecurityTradingStatus?.ToString());
+
+                if (statusMatch != null)
+                {
+                    bond.SecurityTradingStatusText = statusMatch.Description;
+                }
+            }
+
 
             return View(list);
         }
